f6ddc66b8c72afd26a2e232b26f19bdb4f22ad8a
Niels Gerritsen <n.c.gerritsen@gmail.com>
2017-09-09 16:28:20 +0200

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,20 +1,20 @@
-CREATE TABLE cryptocurrencies (
-    tag varchar(8) NOT NULL PRIMARY KEY,
+CREATE TABLE cryptocurrency (
+    currency varchar(8) NOT NULL PRIMARY KEY,
     transaction_cost float,
     logo_url varchar(255),
     name varchar(16)
 );
 
 CREATE TABLE cryptocurrency_price (
-    tag varchar(6) NOT NULL,
+    currency varchar(6) NOT NULL,
     time timestamp  NOT NULL,
     sell float NOT NULL,
     buy float NOT NULL,
-    PRIMARY KEY (tag, time)
+    PRIMARY KEY (tag, time),
+    FOREIGN KEY (currency) REFERENCES cryptocurrency(currency)
 );
 
 CREATE TABLE bitcoin_price (
-    tag varchar(6) NOT NULL,
     time timestamp  NOT NULL,
     sell float NOT NULL,
     buy float NOT NULL,
